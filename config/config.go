1e029907da8054362f94e3a224d9b8060589f6b8
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-11-30 02:14:20 -0500

    chore: remove unused github_token config var

--- config/config.go
+++ config/config.go
@@ -24,9 +24,6 @@ type Config struct {
 	// GithubScopes is the list of scopes to request from the GitHub API
 	GithubScopes []string
 
-	// GithubToken is the token used to authenticate with the GitHub API
-	GithubToken string
-
 	// JWTSecret is the secret used to sign the JWT tokens
 	JWTSecret string
 
@@ -40,7 +37,6 @@ type Config struct {
 func NewConfig(database *gorm.DB, staticFiles embed.FS) Config {
 	config := Config{
 		Database:           database,
-		GithubToken:        os.Getenv("GITHUB_TOKEN"),
 		GithubRedirectURL:  os.Getenv("GITHUB_REDIRECT_URL"),
 		GithubClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		GithubClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
@@ -60,10 +56,6 @@ func NewConfig(database *gorm.DB, staticFiles embed.FS) Config {
 
 	if config.GithubClientSecret == "" {
 		log.Fatal("GITHUB_CLIENT_SECRET environment variable is required")
-	}
-
-	if config.GithubToken == "" {
-		log.Fatal("GITHUB_TOKEN environment variable is required")
 	}
 
 	if config.JWTSecret == "" {
