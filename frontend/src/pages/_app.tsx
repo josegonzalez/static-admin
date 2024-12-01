0bfbe07111e80226ebcee89eb498cd5e9d2e4721
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2024-12-01 05:39:56 -0500

    fix: correct issues with rendering accidentally rendering the homepage on refresh
    See this long discussion for more details: https://github.com/vercel/next.js/discussions/10522#discussioncomment-2304314

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 import type { NextPage } from "next";
+import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 import type { ReactElement } from "react";
+import { useEffect, useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactElement;
@@ -13,5 +19,20 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  const [ready, setReady] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    if (router.isReady) {
+      setReady(true);
+    }
+  }, [router, router.isReady]);
+  return (
+    ready && (
+      <ThemeProvider attribute="class">
+        <main className={inter.className}>
+          {getLayout(<Component {...pageProps} />)}
+        </main>
+      </ThemeProvider>
+    )
+  );
 }
